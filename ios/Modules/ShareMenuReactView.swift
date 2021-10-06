87e3fe0f14026a6d43ceee6251569c4097718ad6
Ruben Fernandez-Butron <ruben.fernandez@moonsift.com>
2021-10-06 16:15:37 +0100

    fix: update native extractDataFromContext get the url attachment only

--- ios/Modules/ShareMenuReactView.swift
+++ ios/Modules/ShareMenuReactView.swift
@@ -105,15 +105,6 @@ public class ShareMenuReactView: NSObject {
             if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
                 urlProvider = provider as? NSItemProvider
                 break
-            } else if provider.hasItemConformingToTypeIdentifier(kUTTypeText as String) {
-                textProvider = provider as? NSItemProvider
-                break
-            } else if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                imageProvider = provider as? NSItemProvider
-                break
-            } else if provider.hasItemConformingToTypeIdentifier(kUTTypeData as String) {
-                dataProvider = provider as? NSItemProvider
-                break
             }
         }
 
@@ -125,7 +116,7 @@ public class ShareMenuReactView: NSObject {
             }
         } else if (imageProvider != nil) {
             imageProvider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil) { (item, error) in
-                let imageUrl: URL! = item as? URL
+                let imageUrl: NSURL! = item as? NSURL
 
                 if (imageUrl != nil) {
                     if let imageData = try? Data(contentsOf: imageUrl) {
